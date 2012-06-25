92005171f701706daa1ac107b7b2b4dcd35865c9
Sabrina Staedt & Trace Wax <pair+sstaedt+twax@pivotallabs.com>
2012-06-25 15:59:35 -0400

    WIP - Spinner.js replacement using sonic canvas

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,7 +3,7 @@
 //= require twitter
 //= require versionCheck
 //= require projectCheck
-//= require spin
+//= require sonic
 
 $(function() {
   VersionCheck.init();
@@ -20,7 +20,8 @@ $.fn.setSpinner = function(projectsCount){
   var $this = $(this);
 
   if (projectsCount == 15) {
-    $this.spin({radius:8, length:9, width:3, lines:12, top:2, left:16});
+    // $this.spin(radius:8, length:9, width:3, lines:12, top:2, left:16});
+    $this.spin({diameter: 60, lineWidth: 1.5, innerRadius: 10});
   }
   else if (projectsCount == 24) {
     $this.spin({radius:6, length:7, width:2, lines:12, top:4, left:6});
@@ -35,16 +36,69 @@ $.fn.setSpinner = function(projectsCount){
 
 $.fn.spin = function(opts) {
   this.each(function() {
-    var $this = $(this),
-        data = $this.data();
+    var $this = $(this);
+    var diameter = opts.diameter;
+    var c = Math.floor(diameter/3);
+    var lineWidth = opts.lineWidth;
 
-    if (data.spinner) {
-      data.spinner.stop();
-      delete data.spinner;
-    }
+    $this.find(".spinner").remove();
+
     if (opts !== false) {
-      data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+      var loader =
+        {
+
+        width: diameter, //75,//* diameter
+        height: diameter, //75,//*
+
+        stepsPerFrame: 1,
+        trailLength: 1,
+        pointDistance: .05,
+
+        //strokeColor: '#FF2E82',
+        strokeColor: '#FF',
+        fps: 20,
+
+        setup: function() {
+          this._.lineWidth = lineWidth; //2;//*
+        },
+        step: function(point, index) {
+
+          var cx = c;//this.padding + Math.floor(diameter/3), //25,//*
+          cy = c;//this.padding + Math.floor(diameter/3), //25,//*
+          _ = this._,
+          angle = (Math.PI/180) * (point.progress * 360),
+          innerRadius = opts.innerRadius;//*12
+
+          _.beginPath();
+          _.moveTo(point.x, point.y);
+          _.lineTo(
+            (Math.cos(angle) * innerRadius) + cx,
+            (Math.sin(angle) * innerRadius) + cy
+          );
+          _.closePath();
+          _.stroke();
+
+        },
+        path: [
+          ['arc', c, c, c * .8, 0, 360]//*
+        ]
+      };
+
+      var d, a;
+
+
+      d = document.createElement('div');
+      d.className = 'spinner';
+
+      a = new Sonic(loader);
+
+      d.appendChild(a.canvas);
+      this.appendChild(d);
+
+      a.canvas.style.marginTop = (diameter - a.fullHeight) / 2 + 'px';//*
+      a.canvas.style.marginLeft = (diameter - a.fullWidth) / 2 + 'px';//*
+
+      a.play();
     }
   });
-  return this;
-};
+}
